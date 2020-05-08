dd635c08ca15949a5469f986a6bb505a2545e061
Maxim Vashkevich <mvashkewi4@yandex.ru>
2020-05-09 00:25:15 +0300

    Remove declarations and styles from index.phtml

--- code/modules/dateParser.php
+++ code/modules/dateParser.php
@@ -5,15 +5,12 @@ namespace DateParser;
 const USA_DATE_REGEXP = '/\b(0?[1-9]|1[0-2])\/(0?[1-9]|[12]\d|3[01])\/(\d{4}|\d{2})\b/m';
 const GOST_DATE_REGEXP = '/\b(3[01]|0?[1-9]|[12]\d)\.(0?[1-9]|1[0-2])\.(\d{4}|\d{2})\b/m';
 const MAX_TWO_DIGIT_YEAR = 99;
-const TWO_DIGIT_YEAR_EQUAL_2000 = 100;
 
 function increaseYear($year)
 {
     $increasedYear = $year + 1;
-    if ($increasedYear == TWO_DIGIT_YEAR_EQUAL_2000) {
-        $year = 2000;
-    }
-    return ($increasedYear <= MAX_TWO_DIGIT_YEAR) ? sprintf("%02d", $year) : $year;
+    var_dump($increasedYear);
+    return ($increasedYear <= MAX_TWO_DIGIT_YEAR) ? sprintf("%02d", $increasedYear) : $increasedYear;
 }
 
 function parseDates(string $text)
@@ -27,4 +24,15 @@ function parseDates(string $text)
             . $match[1] . '.' . increaseYear($match[3]) . '</span>';
     }, $text);
     return $text;
+}
+
+function printTable($source, $modified)
+{
+    echo '<div class="compare"><pre class="compare__item">';
+    echo $source;
+    echo '</pre>';
+    echo '<pre class="compare__item">';
+    echo $modified;
+    echo '</pre>';
+    echo '</div>';
 }
