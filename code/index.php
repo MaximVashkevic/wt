cb596f489b179d49138c4c3b531296625c6eb42e
Maxim Vashkevich <mvashkewi4@yandex.ru>
2020-05-09 15:06:13 +0300

    init

--- code/index.php
+++ code/index.php
@@ -1,2 +1,9 @@
 <?php
-phpinfo();
+session_start();
+$_SESSION['visits'][] = new DateTime();
+
+foreach ($_SESSION['visits'] as $visit)
+{
+    echo $visit->format('r') . '<br>';
+}
+echo 'Total ' . count($_SESSION['visits']) . ' visits';
