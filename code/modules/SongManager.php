5f978a943069cd946a721032ef655969172501dd
Maxim Vashkevich <mvashkewi4@yandex.ru>
2020-05-24 12:50:39 +0300

    Add chord playing

--- code/modules/SongManager.php
+++ code/modules/SongManager.php
@@ -29,87 +29,87 @@ class SongManager
                     'name' => 'Кукушка',
                     'text' =>
                         <<<'END'
-Вступление: <b>Am</b> | <b>C</b> | <b>G</b> | <b>F</b>
+Вступление: <b class="chord">Am</b> | <b class="chord">C</b> | <b class="chord">G</b> | <b class="chord">F</b>
 
-<b>Am</b>                           <b>C</b>
+<b class="chord">Am</b>                           <b class="chord">C</b>
 Песен, еще не написанных, сколько?
-<b>G</b> <b>Dm</b>
+<b class="chord">G</b> <b class="chord">Dm</b>
 Скажи, кукушка,
-<b>Am</b>
+<b class="chord">Am</b>
 Пропой.
-<b>Am</b>                            
+<b class="chord">Am</b>                            
 В городе мне жить или на выселках?
-<b>C</b>
+<b class="chord">C</b>
 Камнем лежать
-<b>G</b> <b>Dm</b>
+<b class="chord">G</b> <b class="chord">Dm</b>
 Или гореть звездой?
-<b>Am</b>
+<b class="chord">Am</b>
 Звездой...
 
 Припев:
-<b>G</b>          <b>F</b>               <b>Am</b>
+<b class="chord">G</b>          <b class="chord">F</b>               <b class="chord">Am</b>
 Солнце мое, взгляни на меня:
-<b>G</b>       <b>F</b>                     <b>Am</b>
+<b class="chord">G</b>       <b class="chord">F</b>                     <b class="chord">Am</b>
 Моя ладонь превратилась в кулак.
-<b>G</b>             <b>F</b>            <b>Am</b> <b>G</b> <b>F</b>
+<b class="chord">G</b>             <b class="chord">F</b>            <b class="chord">Am</b> <b class="chord">G</b> <b class="chord">F</b>
 И если есть порох, дай огня.
-<b>Am</b>
+<b class="chord">Am</b>
 Вот так.
 
-Проигрыш: <b>Am</b> | <b>C</b> | <b>G</b> | <b>F</b>
+Проигрыш: <b class="chord">Am</b> | <b class="chord">C</b> | <b class="chord">G</b> | <b class="chord">F</b>
 
-<b>Am</b>              
+<b class="chord">Am</b>              
 Кто пойдет по следу одинокому?
-<b>C</b>                                  <b>G</b> <b>Dm</b>
+<b class="chord">C</b>                                  <b class="chord">G</b> <b class="chord">Dm</b>
 Сильные да смелые головы сложили в поле,
-<b>Am</b>              
+<b class="chord">Am</b>              
 В бою.
-<b>Am</b>              
+<b class="chord">Am</b>              
 Мало кто остался в светлой памяти,
-<b>C</b>                                     <b>G</b> <b>Dm</b>
+<b class="chord">C</b>                                     <b class="chord">G</b> <b class="chord">Dm</b>
 В трезвом уме да с твердой рукой в строю.
-<b>Am</b>              
+<b class="chord">Am</b>              
 В строю.
 
 Припев:
-<b>G</b>          <b>F</b>               <b>Am</b>
+<b class="chord">G</b>          <b class="chord">F</b>               <b class="chord">Am</b>
 Солнце мое, взгляни на меня:
-<b>G</b>       <b>F</b>                     <b>Am</b>
+<b class="chord">G</b>       <b class="chord">F</b>                     <b class="chord">Am</b>
 Моя ладонь превратилась в кулак.
-<b>G</b>             <b>F</b>            <b>Am</b> <b>G</b> <b>F</b>
+<b class="chord">G</b>             <b class="chord">F</b>            <b class="chord">Am</b> <b class="chord">G</b> <b class="chord">F</b>
 И если есть порох, дай огня.
-<b>Am</b>
+<b class="chord">Am</b>
 Вот так.
 
-<b>Am</b>                  
+<b class="chord">Am</b>                  
 Где же ты теперь, воля вольная,
-<b>C</b>                                        <b>G</b>  <b>Dm</b>
+<b class="chord">C</b>                                        <b class="chord">G</b>  <b class="chord">Dm</b>
 С кем же ты сейчас ласковый рассвет встречаешь?
-<b>Am</b>              
+<b class="chord">Am</b>              
 Ответь!
-<b>Am</b>                 
+<b class="chord">Am</b>                 
 Хорошо с тобой да плохо без тебя.
-<b>C</b>                              <b>G</b>  <b>Dm</b>
+<b class="chord">C</b>                              <b class="chord">G</b>  <b class="chord">Dm</b>
 Голову да плечи терпеливые под плеть.
-<b>Am</b>              
+<b class="chord">Am</b>              
 Под плеть.
 
 Припев: 
-<b>G</b>          <b>F</b>               <b>Am</b>
+<b class="chord">G</b>          <b class="chord">F</b>               <b class="chord">Am</b>
 Солнце мое, взгляни на меня:
-<b>G</b>       <b>F</b>                     <b>Am</b>
+<b class="chord">G</b>       <b class="chord">F</b>                     <b class="chord">Am</b>
 Моя ладонь превратилась в кулак.
-<b>G</b>             <b>F</b>            <b>Am</b> <b>G</b> <b>F</b>
+<b class="chord">G</b>             <b class="chord">F</b>            <b class="chord">Am</b> <b class="chord">G</b> <b class="chord">F</b>
 И если есть порох, дай огня.
-<b>Am</b>
+<b class="chord">Am</b>
 Вот так.
-<b>G</b>          <b>F</b>               <b>Am</b>
+<b class="chord">G</b>          <b class="chord">F</b>               <b class="chord">Am</b>
 Солнце мое, взгляни на меня:
-<b>G</b>       <b>F</b>                     <b>Am</b>
+<b class="chord">G</b>       <b class="chord">F</b>                     <b class="chord">Am</b>
 Моя ладонь превратилась в кулак.
-<b>G</b>             <b>F</b>            <b>Am</b> <b>G</b> <b>F</b>
+<b class="chord">G</b>             <b class="chord">F</b>            <b class="chord">Am</b> <b class="chord">G</b> <b class="chord">F</b>
 И если есть порох, дай огня.
-<b>Am</b>
+<b class="chord">Am</b>
 Вот так.
 END
                 ],
@@ -119,45 +119,45 @@ END
                     'name' => 'Там, где клён шумит',
                     'text' =>
                         <<<'END'
-Вступление: <b>Am</b> | <b>Am/G</b> | <b>Am/F#</b> | <b>Am/F</b> <b>Am/E</b>
+Вступление: <b class="chord">Am</b> | <b class="chord">Am/G</b> | <b class="chord">Am/F#</b> | <b class="chord">Am/F</b> <b class="chord">Am/E</b>
 
-      <b>Am</b>               <b>F</b>
+      <b class="chord">Am</b>               <b class="chord">F</b>
  Там, где клён шумит над речной волной, 
-<b>Am</b>           <b>F</b>
+<b class="chord">Am</b>           <b class="chord">F</b>
  Говорили мы о любви с тобой, 
- <b>Dm</b>              <b>G</b>            <b>C</b>
+ <b class="chord">Dm</b>              <b class="chord">G</b>            <b class="chord">C</b>
  Отшумел тот клён, в поле бродит мгла, 
- <b>Dm</b>               <b>Am</b>        <b>E7</b>   <b>Am</b>
+ <b class="chord">Dm</b>               <b class="chord">Am</b>        <b class="chord">E7</b>   <b class="chord">Am</b>
  А любовь, как сон, стороной прошла. 
-          <b>D</b>        <b>G</b>        <b>C</b>
+          <b class="chord">D</b>        <b class="chord">G</b>        <b class="chord">C</b>
  А любовь, как сон, а любовь, как сон, 
-<b>Am</b>        <b>B7</b>      <b>F</b>      <b>E7</b> <b>Am</b>
+<b class="chord">Am</b>        <b class="chord">B7</b>      <b class="chord">F</b>      <b class="chord">E7</b> <b class="chord">Am</b>
  А любовь, как сон, стороной прошла. 
  
-      <b>Am</b>               <b>F</b>
+      <b class="chord">Am</b>               <b class="chord">F</b>
  Сердцу очень жаль, что случилось так, 
-      <b>Am</b>               <b>F</b>
+      <b class="chord">Am</b>               <b class="chord">F</b>
  Гонит осень вдаль журавлей косяк. 
- <b>Dm</b>              <b>G</b>            <b>C</b>
+ <b class="chord">Dm</b>              <b class="chord">G</b>            <b class="chord">C</b>
  Четырём ветрам грусть-печаль раздам, 
- <b>Dm</b>               <b>Am</b>        <b>E7</b>  <b>Am</b>
+ <b class="chord">Dm</b>               <b class="chord">Am</b>        <b class="chord">E7</b>  <b class="chord">Am</b>
  Не вернётся вновь это лето к нам.
-          <b>D</b>        <b>G</b>         <b>C</b> 
+          <b class="chord">D</b>        <b class="chord">G</b>         <b class="chord">C</b> 
  Не вернётся вновь, не вернётся вновь, 
-<b>Am</b>        <b>B7</b>      <b>F</b>      <b>E7</b> <b>Am</b>
+<b class="chord">Am</b>        <b class="chord">B7</b>      <b class="chord">F</b>      <b class="chord">E7</b> <b class="chord">Am</b>
  Не вернётся вновь это лето к нам. 
  
-      <b>Am</b>               <b>F</b>
+      <b class="chord">Am</b>               <b class="chord">F</b>
  Ни к чему теперь за тобой ходить, 
-      <b>Am</b>               <b>F</b>
+      <b class="chord">Am</b>               <b class="chord">F</b>
  Ни к чему теперь мне цветы дарить, 
- <b>Dm</b>              <b>G</b>            <b>C</b>
+ <b class="chord">Dm</b>              <b class="chord">G</b>            <b class="chord">C</b>
  Ты любви моей не смогла сберечь, 
- <b>Dm</b>               <b>Am</b>        <b>E7</b> <b>Am</b>
+ <b class="chord">Dm</b>               <b class="chord">Am</b>        <b class="chord">E7</b> <b class="chord">Am</b>
  Поросло травой место наших встреч.
-          <b>D</b>        <b>G</b>     <b>C</b>
+          <b class="chord">D</b>        <b class="chord">G</b>     <b class="chord">C</b>
  Поросло травой, поросло травой, 
-<b>Am</b>        <b>B7</b>      <b>F</b>     <b>E7</b> <b>Am</b>
+<b class="chord">Am</b>        <b class="chord">B7</b>      <b class="chord">F</b>     <b class="chord">E7</b> <b class="chord">Am</b>
  Поросло травой место наших встреч.
 END
                 ]
